01103d9aadb18eb0ef94e84ed79397224bc2ac06
alexfortin <fortin.alex14@gmail.com>
2022-12-22 18:49:15 -0600

    fixed installation functions not installing

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -4,17 +4,24 @@ const butInstall = document.getElementById('buttonInstall');
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
     event.preventDefault()
+    window.deferredPrompt = event
     butInstall.classList.toggle("hidden", false)
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async (event) => {
-    event.prompt()
-    butInstall.toggle("hidden", true)
-    console.log("it works")
+butInstall.addEventListener('click', async () => {
+    const promptEvent = window.deferredPrompt
+    if(!promptEvent){
+        return
+    }
+    promptEvent.prompt()
+    const result = await promptEvent.userChoice
+    window.deferredPrompt = null
+    butInstall.classList.toggle("hidden", true)
 });
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
     console.log(`app installed ${event}`)
+    window.deferredPrompt = null
 });
